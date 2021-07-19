0652d14a71f963d62f35631b250693568fc552d1
JYeonDev <whddus4837@gmail.com>
2021-07-19 22:17:24 +0900

    initial commit

---
+++ boolean.ts
@@ -0,0 +1,9 @@
+let isDone: boolean = false;
+
+isDone = true;
+
+console.log(typeof isDone) // 'boolean'
+
+let isOk: Boolean = true;
+
+let isNotOk: boolean = new Boolean(true); // 객체로 넘어오기 때문에 실행되지 않음
