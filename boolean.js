e33c6eb60deb0659f36a42ff35ce1bd9c45a6b05
JYeonDev <whddus4837@gmail.com>
2021-07-19 22:38:00 +0900

    symbol

--- boolean.js
+++ boolean.js
@@ -3,4 +3,4 @@ var isDone = false;
 isDone = true;
 console.log(typeof isDone); // 'boolean'
 var isOk = true;
-var isNotOk = new Boolean(true); // 객체로 넘어오기 때문에 실행되지 않음
+// var isNotOk = new Boolean(true); // 객체로 넘어오기 때문에 실행되지 않음
--- boolean.ts
+++ boolean.ts
@@ -6,4 +6,4 @@ console.log(typeof isDone) // 'boolean'
 
 let isOk: Boolean = true;
 
-let isNotOk: boolean = new Boolean(true); // 객체로 넘어오기 때문에 실행되지 않음
+// let isNotOk: boolean = new Boolean(true); // 객체로 넘어오기 때문에 실행되지 않음
