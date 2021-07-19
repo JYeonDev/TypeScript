383bdd1f92662462cb1c689046843652e6e27526
JYeonDev <whddus4837@gmail.com>
2021-07-19 22:27:26 +0900

    number

---
+++ number.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+JavaScript 와 같이, TypeScript 의 모든 숫자는 부동 소수점 값 입니다.
+
+TypeScript는 16진수 및 10진수 리터럴 외에도,
+ECMAScript 2015에 도입된 2진수 및 8진수를 지원합니다.
+
+NaN
+
+1_000_000 과 같은 표기 가능
+*/
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var notAnumber = NaN;
+var uderscoreNum = 1000000;
